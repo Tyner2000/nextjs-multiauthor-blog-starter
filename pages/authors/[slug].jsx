65d0eca2894a2b0a87514f948453537abf5dc990
Tyner Roycroft <tynersemail@gmail.com>
2023-11-15 08:53:34 -0500

    fix error

--- pages/authors/[slug].jsx
+++ pages/authors/[slug].jsx
@@ -10,7 +10,6 @@ export default function Author({ author }) {
       <div className="author_page_container">
         <div className="author_page_contents">
           <h1 className={titleFont.className}>{author.name}</h1>
-          <p className={textFont.className}>
           <p className={textFont.className}>👋 Hello!</p>
           <p className={textFont.className}>My name is Tyner. I live in North Carolina, US.</p>
           <p className={textFont.className}>This website is a slow-growing collection of written works that I have created since 2020. This blog serves friends and family predominately. I make no attempt to market or publish anything relating to this website anywhere. If you have stumbled across this page: Welcome!</p>
