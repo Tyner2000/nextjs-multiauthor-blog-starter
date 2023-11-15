352591fff64be4b552cdc3f3ccace99a257189b4
Tyner Roycroft <tynersemail@gmail.com>
2023-11-15 08:22:43 -0500

    fix index ui

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,17 +11,17 @@ export default function Home() {
         <meta name="description" content="Tyner Roycroft's Writing." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="main">
+      <div className="index_container">
         <div className="index_title">
           <h1 className={titleFont.className}>Works Collected.</h1>
         </div>
-        <div className={styles.root_index_description}>
+        <div className="root_index_description">
           <p>👋 Hello!</p>
           <p>My name is Tyner. I live in North Carolina, US.</p>
           <p>This website is a slow-growing collection of written works that I have created since 2020. This blog serves friends and family predominately. I make no attempt to market or publish anything relating to this website anywhere. If you have stumbled across this page: Welcome!</p>
           <p>Since you're already here, go ahead and check out some of my poetry and short stories! If any of you are curious about the development of this blog, you can follow its progress on my Github profile, which is linked in the sidebar of this page.</p>
         </div>
-      </main>
+      </div>
     </div>
   )
 }
